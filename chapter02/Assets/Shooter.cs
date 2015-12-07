18bb72aff7706ebc007a8c8b80a0778891a88866
wanghonggang <wanghonggang@hoolai.com>
2015-12-07 21:27:54 +0800

    chapter02

---
+++ chapter02/Assets/Shooter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//可直接拖拽脚本文件到要作用的游戏对象上
+public class Shooter : MonoBehaviour {
+
+	public Rigidbody bullet;
+	public float power = 1500f;
+	public float moveSpeed = 2f;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		//Input.GetAxis返回-1到1之间的小数
+		float hVal = Input.GetAxis ("Horizontal");
+		//print("hVal="+hVal);
+		float h = hVal * Time.deltaTime * moveSpeed;
+
+		float vVal = Input.GetAxis ("Vertical");
+		//print("vVal="+vVal);
+		float v = vVal * Time.deltaTime * moveSpeed;
+
+		//transform可以直接被使用是因为每个游戏对象都有Transform组件，Vector3是形如(x,y,z)的三维向量
+		transform.Translate (h, v, 0);
+		if(Input.GetButtonUp("Fire1")){
+			//使用Instantiate创建（克隆）预设（模板）对象，as是C#里面做声明转换
+			Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+			//使用TransformDirection把本地坐标转换为世界坐标，此处Vector3.forward为(0,0,1)代表z轴1个单位（即前方）
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			//使用Rigidbody.AddForce为刚体添加作用力，力的方向由其参数Vector3指定，此处即代表正前方*power的力
+			instance.AddForce(fwd * power);
+		}
+	}
+}
---
+++ chapter02/Assets/ShooterJS.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+//可直接拖拽脚本文件到要作用的游戏对象上
+
+var bullet:Rigidbody;
+var power:float = 1500;
+var moveSpeed:float = 5;
+
+function Start () {
+
+}
+
+function Update () {
+	//Input.GetAxis返回-1到1之间的小数
+	var hVal:float = Input.GetAxis ("Horizontal");
+	//print("hVal="+hVal);
+	var h:float = hVal * Time.deltaTime * moveSpeed;
+	
+	var vVal:float = Input.GetAxis ("Vertical");
+	//print("vVal="+vVal);
+	var v:float = vVal * Time.deltaTime * moveSpeed;
+	
+	//transform可以直接被使用是因为每个游戏对象都有Transform组件，Vector3是形如(x,y,z)的三维向量
+	transform.Translate (h, v, 0);
+	if(Input.GetButtonUp("Fire1")){
+		//使用Instantiate创建（克隆）预设（模板）对象，as是C#里面做声明转换
+		var instance:Rigidbody = Instantiate(bullet, transform.position, transform.rotation);
+		//使用TransformDirection把本地坐标转换为世界坐标，此处Vector3.forward为(0,0,1)代表z轴1个单位（即前方）
+		var fwd:Vector3 = transform.TransformDirection(Vector3.forward);
+		//使用Rigidbody.AddForce为刚体添加作用力，力的方向由其参数Vector3指定，此处即代表正前方*power的力
+		instance.AddForce(fwd * power);
+	}
+}
