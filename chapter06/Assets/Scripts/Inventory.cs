ece4f23736df2aa733614357fd7430446fb686d4
wanghonggang <wanghonggang@hoolai.com>
2015-12-23 03:13:20 +0800

    使用Canvas和RawImages替代了不会显示纹理图片的GUITexture

--- chapter06/Assets/Scripts/Inventory.cs
+++ chapter06/Assets/Scripts/Inventory.cs
@@ -7,9 +7,23 @@ public class Inventory : MonoBehaviour {
 
 	public AudioClip collectSound;
 
+	// HUD——代表电力的纹理图片数组
+	public Texture2D[] hudCharge;
+
+	// TODO 去除掉chargeHudGUI，因为按照书上的去添加GUITexture纹理图片不显示
+	public GUITexture chargeHudGUI;
+
+	// powerImg
+	private UnityEngine.UI.RawImage powerImg;
+
 	// Use this for initialization
 	void Start () {
 		charge = 0;
+
+		//需要了解一下引擎类的继承体系结构了，否则都是靠猜测和查API去写代码
+		GameObject canvas = GameObject.FindWithTag("Canvas");
+		//canvas..transform.FindChild ("powerImg").gameObject;
+		powerImg = canvas.GetComponentInChildren<UnityEngine.UI.RawImage> ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +34,12 @@ public class Inventory : MonoBehaviour {
 	void PickUpCell(){
 		//GetComponent<AudioSource>().PlayOneShot(collectSound);
 		AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
 		charge++;
+
+		//chargeHudGUI.texture = hudCharge[charge];
+		//print(powerImg);
+		powerImg.texture = hudCharge[charge];
 	}
 
 	public static bool IsFinishedCollect(){
