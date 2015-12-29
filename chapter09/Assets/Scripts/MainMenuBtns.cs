e71cdb6fd43bf83111bc9c08d91bda720c761fe7
wanghonggang <wanghonggang@hoolai.com>
2015-12-29 18:32:51 +0800

    update chapter09

---
+++ chapter09/Assets/Scripts/MainMenuBtns.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class MainMenuBtns : MonoBehaviour {
+
+	public string levelToLoad;
+	public Texture2D normalTexture;
+	public Texture2D rollOverTexture;
+	public AudioClip beep;
+
+	// Use this for initialization
+	void Start () {
+		//print("MainMenuBtns Start...");
+	}
+
+	void OnMouseEnter(){
+		//print("MainMenuBtns OnMouseEnter...");
+		GetComponent<GUITexture>().texture = rollOverTexture;
+	}
+
+	void OnMouseExit(){
+		//print("MainMenuBtns OnMouseExit...");
+		GetComponent<GUITexture>().texture = normalTexture;
+	}
+
+	IEnumerator OnMouseUp(){
+		GetComponent<AudioSource>().PlayOneShot(beep);
+		yield return new WaitForSeconds(0.5f);
+		Application.LoadLevelAdditive(levelToLoad);
+	}
+
+}
