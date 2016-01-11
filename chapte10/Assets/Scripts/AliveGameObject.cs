78236a7c8b0308ad6f0817a3eb4566a8254e26a3
wanghonggang <wanghonggang@hoolai.com>
2016-01-11 11:31:15 +0800

    add chapter10, but GUITexture not show...

---
+++ chapte10/Assets/Scripts/AliveGameObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AliveGameObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
