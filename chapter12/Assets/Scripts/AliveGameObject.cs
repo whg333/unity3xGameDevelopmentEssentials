3bca938e06de3aa2f4da3d198874a0604a9ebb61
wanghonggang <wanghonggang@hoolai.com>
2016-01-21 19:29:50 +0800

    update chapter12

--- chapter12/Assets/Scripts/AliveGameObject.cs
+++ chapter12/Assets/Scripts/AliveGameObject.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//用于标记当切换场景的时候需要保留的对象
 public class AliveGameObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-
+		
 	void Awake(){
 		DontDestroyOnLoad(transform.gameObject);
 	}
